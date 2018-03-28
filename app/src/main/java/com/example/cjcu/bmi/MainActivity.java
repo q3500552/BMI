22596fb9173d647c7e01b2cfe8fe5ef6ad643a00
q3500552 <q3500552@gmail.com>
2018-03-28 15:05:24 +0800

    第二版

--- app/src/main/java/com/example/cjcu/bmi/MainActivity.java
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cjcu.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         float bmi =w/(h*h);
         Log.d(  "BMI",String.valueOf(bmi));
         Toast.makeText(this,String.valueOf(bmi),Toast.LENGTH_SHORT).show();
+
+        new AlertDialog.Builder(this)
+                .setMessage("BMI="+bmi)
+                .setTitle("BMI值")
+                .setPositiveButton("確定",null)
+                .setPositiveButton("CANCEL",null)
+                .show();
 
     }
 }
